613bad3066cd609a793a7ba1d2ceb128863d725c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-17 02:08:36 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,11 +1,14 @@
 class Pirate
   attr_accessor :name, :weight, :height
 
+  PIRATES= []
+
   def initialize(name, weight, height)
     @name=name
     @weight =weight
     @height = height
-    @@all<<self
+    PIRATES<<self 
+    
   end
 
   def self.all
